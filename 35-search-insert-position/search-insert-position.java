fe72754425500fde88ab28a099eba41beb91a416
Naresh Gujrati <nareshgujrati96@gmail.com>
2026-06-05 01:19:37 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (64.70%) - LeetSync

--- 35-search-insert-position/search-insert-position.java
+++ 35-search-insert-position/search-insert-position.java
@@ -3,19 +3,18 @@ class Solution {
         int left = 0;
         int right = nums.length - 1;
 
-        while (left <= right) {
+        while(left <= right){
             int mid = left + (right - left) / 2;
 
-            if (nums[mid] == target) {
+            if (nums[mid] == target){
                 return mid;
-            } else if (nums[mid] > target) {
-                
+
+            } else if (nums[mid] > target){
                 right = mid - 1;
-            } else {
+            }else{
                 left = mid + 1;
             }
         }
-
-        return left;        
+        return left;
     }
 }
